04c98157031912768567955c4bc1d2c07dba176a
root <ellisarmahayikwei@gmail.com>
2023-03-18 20:13:45 -0700

    'alpha'

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,13 @@
 *Return: Always 0 (Success)
 */
 
-int main(void){
-
+int main(void)
+	{
 	char letas;
-	
-	for( letas = 'a' ; letas <= 'z' ; letas++)
+
+	for (letas = 'a' ; letas <= 'z' ; letas++)
 	{
 		putchar(letas);
-		putchar('\n');
 	}
 	return (0);
 }
