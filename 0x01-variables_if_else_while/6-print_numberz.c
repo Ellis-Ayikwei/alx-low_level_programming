1e2b7867197f968083c83754728d53a32cf32a1b
root <ellisarmahayikwei@gmail.com>
2023-03-18 21:05:26 -0700

    'number from 1 t0'

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/* more headers goes there */
+
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+	{
+	char letas;
+
+	for (letas = 1 ; letas <= 10 ; letas++)
+	{
+		putchar(letas);
+	}
+		putchar('\n');
+	return (0);
+}
