880ab34720205a53eac00017ccb270dcad4f7ca8
root <ellisarmahayikwei@gmail.com>
2023-03-19 09:42:21 -0700

    'The success combination in business is: Do what you do better... and: do more of what you do..'

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+/* more headers goes there */
+
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+	{
+	int letas, logan;
+
+	letas = 0;
+	logan = 0;
+
+
+	for (letas = 0 ; letas <= 8 ; letas++)
+	{
+		for(logan = letas + 1 ; logan <= 9 ; logan++ )
+		{
+			if (letas != logan)
+			{
+				putchar('0' + letas);
+				putchar('0' + logan);
+			}
+			if(letas <=8 && logan <=9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+	}
+		putchar('\n');
+		
+
+	return (0);
+}
