543ba88aa7ca8e15141c33796f9e12b17e36c753
root <ellisarmahayikwei@gmail.com>
2023-03-19 07:01:55 -0700

    'numbers '

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/* more headers goes there */
+
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+	{
+	int letas;
+
+	for (letas = 0 ; letas <= 10 ; letas++)
+	{
+		printf("%d", letas);
+	}
+		putchar('\n');
+	return (0);
+}
