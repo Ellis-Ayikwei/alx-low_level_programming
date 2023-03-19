22586f80887ab26188fe7bb2e687f0b95c1f6d2e
root <ellisarmahayikwei@gmail.com>
2023-03-18 20:01:44 -0700

    'alphabets'

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/* more headers goes there */
+
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void){
+
+	char letas;
+	
+	for( letas = 'a' ; letas <= 'z' ; letas++)
+	{
+		putchar(letas);
+		putchar('\n');
+	}
+	return (0);
+}
