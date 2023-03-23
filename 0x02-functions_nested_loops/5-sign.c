6ab8769a7d60fe86df1583c76b3bbcd59243bc5a
root <ellisarmahayikwei@gmail.com>
2023-03-22 20:28:27 -0700

    'number signs '

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_sign - Print and checks if a character is negative ,zero ,or positive.
+* * @n: The character to be checked.
+*
+* Return: 1 if number is greater than zero
+*-1 if the number is less than zero,
+*0 if the num ber is 0.
+*/
+int  print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	}
+	else if (n < 0
+	{
+		_putchar('-');
+	}
+	else
+	{
+	_putchar(48);
+	}
+}
