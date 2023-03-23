c75af8070a4cb912e8e7e8209c32774585c5cd5e
root <ellisarmahayikwei@gmail.com>
2023-03-22 22:10:52 -0700

    'functions '

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,9 @@ int _islower();
 int _isalpha();
 int _abs();
 int  print_sign();
+int print_last_digit();
+void jack_bauer();
+void times_table();
+int add(int num1, int num2);
+void print_to_98();
 #endif /* MAIN_H */
