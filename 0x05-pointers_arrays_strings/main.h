ad145ea511f798598751a1cd3020538e8028badf
root <ellisarmahayikwei@gmail.com>
2023-03-27 21:53:06 -0700

    'first on pinters'

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_number(int n);
+int _isupper(int c);/*finds the uppercase letters*/
+int _isdigit(int c);
+void print_triangle(int size);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void more_numbers(void);
+int _putchar(char c);
+void print_alphabet();
+void print_alphabet_x10();
+int _islower();
+int _isalpha();
+int _abs();
+int  print_sign();
+int print_last_digit();
+void jack_bauer();
+void times_table();
+int add(int num1, int num2);
+void print_to_98();
+
+/*0x05-pointers_arrays_strings*/
+void reset_to_98(int *n);
+#endif /* MAIN_H */
