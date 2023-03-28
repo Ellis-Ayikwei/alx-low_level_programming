b91460caaae95cd51d0d1e303a4a38330ed1ffe1
root <ellisarmahayikwei@gmail.com>
2023-03-27 21:58:34 -0700

    'first on pointers'

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include "main.h"
 #include<stdio.h>
 /**
- * main - check the code 
- *
+ * reset_to_98 - changes agive number to 98
+ *by taking the urinary of the input and then
+*pointing to its value
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-	
 	*n = 98;
-	
-	
 }
