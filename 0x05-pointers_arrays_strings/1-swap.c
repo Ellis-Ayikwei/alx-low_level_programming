5f5c93e1124498928d705cb64980719ff1afcc13
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-03-29 02:25:03 +0000

    'second on first '

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * void swap_int - swapes the inputs of a given numbers 
- *@a: the  value to be checked 
-*@b: the second value to be checked 
+*swap_int - swapes the inputs of a given numbers
+*@a: the  value to be checked
+*@b: the second value to be checked
 *by taking the urinary of the input and then
 *pointing to its value
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
 {
-	int u ;
-	u = *a ;
-	*a = *b ;
-	*b = u ;	
+	int u;
+
+	u = *a;
+	*a = *b;
+	*b = u;
 }
