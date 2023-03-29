d16e4170d5fde23e410d84adae35af27126671bb
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-03-29 10:09:40 +0000

    'on the 9 '

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*print_array - checks a character and reverses it
-*@a: is the pointer of the array a
-*@n: is the size of the array
-*return: Always 0.
+* _strcpy - checks a character and reverses it
+*@dest: the buffer to copy the srtings to
+*@src: the source to copy the string from
+* Return: Always dest.
 */
 char *_strcpy(char *dest, char *src)
 {
 	char *j;
+
 	j = dest;
-	
-	while(*src != '\0'){
+	while (*src != '\0')
+	{
 		*j++ = *src++;
 	}
 	*j = '\0';
-	return dest;
+	return (dest);
 }
