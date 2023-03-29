e3577173da8dd75a9c7b25bf84a3d9daabbb41a5
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-03-29 04:15:44 +0000

    'on the second one '

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * void swap_int - swapes the inputs of a given numbers 
- *@a: the  value to be checked 
-*@b: the second value to be checked 
+* _strlen - swapes the inputs of a given numbers
+ *@s: the  value to be checked
 *by taking the urinary of the input and then
 *pointing to its value
  * Return: Always 0.
  */
 int _strlen(char *s)
-{	
+{
 	int i, b;
 
-	b=0;
-	for(i = 0 ; s[i] != '\0' ; i++)
+	b = 0;
+	for (i = 0 ; s[i] != '\0' ; i++)
 	{
-	b +=1;
+	b += 1;
 	}
-	return(b);
+	return (b);
 }
