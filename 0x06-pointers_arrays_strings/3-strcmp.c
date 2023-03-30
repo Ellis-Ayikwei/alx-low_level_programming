5c7d0b074ab9b7397de652d77e849e1904fc9a28
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-03-30 16:15:20 +0000

    '3'

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,22 @@
 #include "main.h"
 #include <string.h>
 /**
-* _strncpy - copies a string.
-*@dest: the buffer to copy the srtings to
-*@src: the source to copy the string from
-*@n: the size of the cated string.
-* Return: Always dest.
+* _strcmp - compares a string.
+*@s1: the buffer to copy the srtings to
+*@s2: the source to copy the string from.
+* Return:returns  an  integer  less  than,
+*equal  to,  or greater than zero if s1
+*is found, respectively, to be
+*less than, to match, or
+*be greater than s2..
 */
 int _strcmp(char *s1, char *s2)
 {
 
-	while ( *s1 && *s2 && *s1 == *s2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	return(*s1 - *s2);
+	return (*s1 - *s2);
 }
