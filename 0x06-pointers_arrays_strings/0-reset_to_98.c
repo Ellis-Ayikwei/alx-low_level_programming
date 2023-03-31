97f35419894e6cd237e9bd75c6556c9ff2576ef7
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-03-31 02:50:05 +0000

    'onto to the reset'

---
+++ 0x06-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * reset_to_98 - changes agive number to 98
+ *@n: the value thats stores the address location of the input
+*by taking the urinary of the input and then
+*pointing to its value
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
