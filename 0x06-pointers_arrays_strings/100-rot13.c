a9ef599c3271dde0c3186ef195df42b4ae8387a2
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-03-31 02:22:56 +0000

    '1000'

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - rotates the alphabet to the next 13 alphabet.
+ * @str: The string to be be rotated .
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str)
+{
+	int i = 0;
+	 int j;
+
+	char alphas[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char roted[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[i])
+	{
+		for (j = 0 ; j <= 52 ; j++)
+		{
+			if (str[i] == alphas[j])
+			{
+				str[i] = roted[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
+
