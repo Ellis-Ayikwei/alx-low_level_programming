1c6d651f2b25e22288a3af97c51716a02182fcf5
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-04 01:11:06 +0000

    'on the two with the first c '

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strchr - returns a pointer to the first
+*occurrence of the character c in the
+*string s , or NULL if the character is not found.
+*@c: the character to find.
+*@s: the string to check
+*Return: a pointer to the first found charact if
+*not the NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	long i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (0);
+}
