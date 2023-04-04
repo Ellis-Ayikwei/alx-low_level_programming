e14a98d1bba71d8f15460d63452d61cefcddfb0a
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-04 00:23:03 +0000

    'on the 1 to copy '

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,13 +9,13 @@
 *@b: the constant to fill the first address
 *Return: always the (s);
 */
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0 ; i < n ; i++)
 	{
-		s[i] = b;
+		dest[i]= src[i];
 	}
-	return (s);
+	return (dest);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -48,6 +48,7 @@ char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 /*07*/
 
 #endif /* MAIN_H */
