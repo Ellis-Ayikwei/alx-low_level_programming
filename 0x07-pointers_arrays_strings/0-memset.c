8e6665137e63e6622426f7eeddbddab3cae00df2
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-04 00:09:39 +0000

    'on the betty for 0 '

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
-
-
-
-
-
-
+/**
+* _memset - function  fills the
+*first @n: bytes of the memory area
+*pointed to by s with the constant byte c.
+*@s: the pointer whos first memory will be filled
+*@n: the byte to be filled to the first pointer
+*@b: the constant to fill the first address
+*Return: always the (s);
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i ;
+	unsigned int i;
 
 	for (i = 0 ; i < n ; i++)
 	{
-		s[i]=b ;
-	} 
-
-	return(s); 
+		s[i] = b;
+	}
+	return (s);
 }
