d3ab09ccdc5a9d9962515eecb30152357c6c5292
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-05 17:25:46 +0000

    'on the 100 '

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* print_diagsums - prints a  the sum
+*of the diagonal in an array
+*@a: the array whos diagonal will be added
+*@size: is the size of the array
+*
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,6 @@
 */
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-	int j = size - 1;
-	int sum1 = 0;
-	int sum2 = 0;
-
 	while (i <= (size * size))
 	{
 		sum1 = sum1 + a[i];
