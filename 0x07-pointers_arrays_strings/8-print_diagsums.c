5abee627d45d5f5319a241febf700967a917bdef
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-05 15:20:38 +0000

    'on the 8 '

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_chessboard - prints a chessboaord taking the array size
-*by itself
-*@a: the array to be multiplied
+* print_diagsums - prints a  the sum
+*of the diagonal in an array
+*@a: the array whos diagonal will be added
+*@size: is the size of the array
 *
 */
 void print_diagsums(int *a, int size)
 {
 	int i;
 	int sum1, sum2;
+
 	sum1 = 0;
 	sum2 = 0;
-
-	
-	for (i = 0 ; i <size ; i++)
-	/*for (j = 0 ; j < size ; j++)*/
-		{
-			sum1 += *(a + i*size + i);
-			sum2 += *(a + i*size + (size-i-1));
-		}
+	for (i = 0 ; i < size ; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - i - 1));
+	}
 	printf("%d, %d \n", sum1, sum2);
 
 }
