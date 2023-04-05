529ab76bc650906ae0418a16ddb543bef2b3dfbb
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-05 01:09:52 +0000

    'on the three '

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+* _strspn- unction that gets the length of a prefix substring
+*@s: the ctring to be checked .
+*@accept: the bytes of te check the sstrings
+*Return: always the bytes of accpt in the prefix of s
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int acc = sizeof((char)*accept);
+	int o, j = 0;
+
+	for (o = 0 ; o >= 0 && s[o] != '\0' ; o++)
+	{
+		for (i = 0 ; i <= acc ; i++)
+		{
+			if (accept[i] == s[o])
+				j += 1;
+		}
+	}
+	return (j);
+}
