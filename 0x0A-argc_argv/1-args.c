91f54ebc390ac153625371641fa084efc5211ac5
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-07 20:12:17 +0000

    'on the second one '

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - prints the arguments passed to a file
+*@argc: the commands that are passed in the terminal
+*@argv: the array of commands that are
+*passed in the terminaal
+*Return: always
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	(void)argv;
+
+	for (i = 0 ; i <= argc ; i++)
+	{}
+	printf("%d\n", i - 2);
+
+	return (0);
+
