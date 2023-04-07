3856a1f1cf162b1afb1ba14f585aa9e6f509cf96
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-07 19:13:33 +0000

    'first on argvuments to main '

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - the function that prints the name of the file
+*@argc: the commands that are passed in the terminal
+*@argv: the array of commands that are
+*passed in the terminaal
+*Return: always
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i  < argc ; i++)
+	{
+		printf("%s \n", argv[i]);
+	}
+	return (0);
+}
