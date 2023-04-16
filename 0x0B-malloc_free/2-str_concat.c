9e768acfbf3041f6d578f5dfe1fb8c4fa6a95f82
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-16 01:25:03 +0000

    'if NULL is passed, treat it as an empty string'

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,10 +16,10 @@ char *str_concat(char *s1, char *s2)
 	int i, h, j = 0;
 
 	if (s1 == NULL)
-		s1= "";
-	if(s2 == NULL)
+		s1 = "";
+	if (s2 == NULL)
 	{
-		s2= "";
+		s2 = "";
 	}
 	for (i = 0 ; s1[i];  i++)
 	{
