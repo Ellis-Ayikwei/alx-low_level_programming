bdc7e59231330087d9f9dbd85f2a8236b03663fc
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-16 00:56:39 +0000

    '2. He who is not courageous enough to take risks will accomplish nothing in life'

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+*str_concat - concatenates two strings.
+*@s1: a string to be concatted.
+*@s2: a string to be concated
+*Return: NUll if the str is null .
+*/
+
+
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	int i, h, j = 0;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; s1[i];  i++)
+	{
+		j++;
+	}
+	h = j;
+	for (i = 0  ; s2[i]; i++)
+	{
+		h++;
+	}
+	a = malloc(sizeof(char) * h);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0  ; s1[i]; i++)
+	{
+		a[i] = s1[i];
+
+	}
+	for (i = 0 ; s2[i] ; i++)
+		a[j + i] = s2[i];
+	a[h + 1] = '\0';
+	return (a);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -72,4 +72,5 @@ int wildcmp(char *s1, char *s2);
 /*0B*/
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif /* MAIN_H */
