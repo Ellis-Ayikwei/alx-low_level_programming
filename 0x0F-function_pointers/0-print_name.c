6661302dc1bee377539f0db4ab43390a61ff9710
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-21 03:17:28 +0000

    'v2 on 0'

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include "function_pointers.h"
 #include <stdio.h>
 /**
   *print_name - prints a name.
@@ -9,5 +8,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
