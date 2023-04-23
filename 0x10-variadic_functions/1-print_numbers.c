3db26a32f2f3e33544b2abf6923c186ce51829e4
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-23 01:03:11 +0000

    'the v3 on print the arguements '

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,13 +19,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator != NULL)
-		for (i = 0 ; i < n ; i++)
-		{
-			printf("%d", va_arg(args, int));
-			if (i != n - 1)
-				printf("%c ", *separator);
-		}
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
 	printf("\n");
 	va_end(args);
 }
