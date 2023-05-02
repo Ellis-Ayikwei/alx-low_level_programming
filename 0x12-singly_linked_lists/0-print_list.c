a282603e984b59bc91ca766cb22f1b12874d56b9
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-05-02 12:47:42 +0000

    'first on the 0 , '

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The head of the list_t list.
+ *
+ * Return: The number of nodes in the list_t list.
+ */
+
+
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+
