0407636ee26b3878b96f99e2d5aa00b63f108608
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-05-03 13:08:26 +0000

    'addnode from the beginning v1 '

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -41,5 +41,6 @@ typedef struct listint_s
 /* 0x13 */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif /* LIST_H */
 
