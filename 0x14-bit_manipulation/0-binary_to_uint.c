9fcbe858337cbe80e850f00264bcfee1d44dbe31
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-05-07 21:32:19 +0000

    '0 v1'

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,15 +16,21 @@ unsigned int binary_to_uint(const char *b)
 	int len;
 
 	if (*b == '\0')
+	{
 		return (0);
+	}
 
 	for (len = 0; b[len];)
+	{
 		len++;
+	}
 
 	for (len -= 1; len >= 0; len--)
 	{
 		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
 
 		bin1 += (b[len] - '0') * bin2;
 		bin2 *= 2;
