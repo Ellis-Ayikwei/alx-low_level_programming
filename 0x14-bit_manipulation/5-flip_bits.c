03da9926a090ddd6aea99e77cf7e82d0475d5258
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-05-17 07:51:08 +0000

    '5 v3'

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * flip_bin -returns the number of bin you
- * :%would need to flip to get from one number to another.
+ * flip_bits - returns the number of
+ * bits you would need to flip to get from one number to another.
  * @n: The number.
  * @m: The number to flip n to.
  *
  * Return: The necessary number of bin to flip to get from n to m.
  */
-unsigned int flip_bin(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int excl = n ^ m, bin = 0;
 
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bin(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif /* MAIN_H */
 
