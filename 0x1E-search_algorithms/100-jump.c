f6ab47ce0866ebd085e3db3ecd08bf67fe67abd0
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-05-08 13:11:46 +0000

    'edited the header file'

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,4 @@
-#include "search_algos"
+#include "search_algos.h"
 /**
   * jump_search - Searches for a value in a sorted array
   *               of integer.
@@ -16,7 +16,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t i, jump, step;
 
 	if (array == NULL || size == 0)
-		return (-1);
+		returint jump_search(int *array, size_t size, int value)n (-1);
 
 	step = sqrt(size);
 	for (i = jump = 0; jump < size && array[jump] < value;)
